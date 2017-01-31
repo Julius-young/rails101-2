caa81db1ff2329ed1f4f2591c0b96e0147963a06
juliusyoung <yangzuohui@gmail.com>
2017-01-31 16:01:52 +0800

    implement groups#new

--- db/schema.rb
+++ db/schema.rb
@@ -14,9 +14,9 @@ ActiveRecord::Schema.define(version: 20170131055126) do
 
   create_table "groups", force: :cascade do |t|
     t.string   "title"
-    t.text     "desciption"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.text     "description"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
   end
 
 end
